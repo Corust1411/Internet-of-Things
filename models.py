c3d116c2cdefc3ef76ab653bba9d5fabd210923b
Corust1411 <73009425+Corust1411@users.noreply.github.com>
2024-07-18 15:18:09 +0700

    Update models.py
    update : add book model and function back in

--- models.py
+++ models.py
@@ -3,6 +3,16 @@ from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 
 from database import Base
 
+
+class Book(Base):
+    __tablename__ = 'books'
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    author = Column(String, index=True)
+    year = Column(Integer, index=True)
+    is_published = Column(Boolean, index=True)
+
 class Student(Base):
     __tablename__ = 'students'
 
